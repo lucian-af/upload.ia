6594fd798320715f0a6f3ddd7388f11a0d62148f
Lucian AF <lucian_af@hotmail.com>
2023-09-15 23:16:56 -0300

    feat: adicionado rota para gerar resumo da transcrição do vídeo

---
+++ upload-ia-api/src/routes/post-generate-ai-completion.ts
@@ -0,0 +1,38 @@
+import { openai } from "../lib/openai";
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
+import { createReadStream } from "node:fs";
+
+const bodySchema = z.object({
+  videoId: z.string().uuid(),
+  template: z.string(),
+  temperature: z.number().min(0).max(1).default(0.5)
+})
+
+export async function postGenerateIaCompletionRoute(app: FastifyInstance) {
+  app.post('/ai/complete', async (request, reply) => {    
+    const { videoId, template, temperature } = bodySchema.parse(request.body)
+    
+    const video = await prisma.video.findUniqueOrThrow({
+      where: { id: videoId }
+    })
+
+    if (!video.transcription)
+      return reply.status(400).send({ error: "Video transcription was not generated yet."})
+
+    const promptMessage = template.replace('{transcription}', video.transcription)
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature: temperature,
+      messages: [
+        { 
+          role: 'user',
+          content: promptMessage
+        }
+      ]
+    })
+
+    return response
+  })
+}
--- upload-ia-api/src/server.ts
+++ upload-ia-api/src/server.ts
@@ -1,14 +1,20 @@
-import { prisma } from "./lib/prisma";
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { postUploadVideoRoute } from "./routes/post-upload-video";
 import { postCreateTranscriptionRoute } from "./routes/post-create-transcription";
+import { postGenerateIaCompletionRoute } from "./routes/post-generate-ai-completion";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAllPromptsRoute);
 app.register(postUploadVideoRoute);
 app.register(postCreateTranscriptionRoute);
+app.register(postGenerateIaCompletionRoute);
 
 app.listen({
   port: 3333
