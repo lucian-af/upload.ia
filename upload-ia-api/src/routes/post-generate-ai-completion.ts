eed5d45a1ffad044498dd9f94a5e57520c4c6a43
Lucian AF <lucian_af@hotmail.com>
2023-09-17 19:12:52 -0300

    feat(post-generate-ai-completion): refactor da rota para fazer stream dos dados gerados

--- upload-ia-api/src/routes/post-generate-ai-completion.ts
+++ upload-ia-api/src/routes/post-generate-ai-completion.ts
@@ -2,37 +2,45 @@ import { openai } from "../lib/openai";
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
-import { createReadStream } from "node:fs";
+import { streamToResponse, OpenAIStream } from 'ai'
 
 const bodySchema = z.object({
   videoId: z.string().uuid(),
-  template: z.string(),
+  prompt: z.string(),
   temperature: z.number().min(0).max(1).default(0.5)
 })
 
 export async function postGenerateIaCompletionRoute(app: FastifyInstance) {
-  app.post('/ai/complete', async (request, reply) => {    
-    const { videoId, template, temperature } = bodySchema.parse(request.body)
-    
+  app.post('/ai/complete', async (request, reply) => {
+    const { videoId, prompt, temperature } = bodySchema.parse(request.body)
+
     const video = await prisma.video.findUniqueOrThrow({
       where: { id: videoId }
     })
 
     if (!video.transcription)
-      return reply.status(400).send({ error: "Video transcription was not generated yet."})
+      return reply.status(400).send({ error: "Video transcription was not generated yet." })
 
-    const promptMessage = template.replace('{transcription}', video.transcription)
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo-16k',
       temperature: temperature,
       messages: [
-        { 
+        {
           role: 'user',
           content: promptMessage
         }
-      ]
+      ],
+      stream: true,
+
     })
 
-    return response
+    const stream = OpenAIStream(response)
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        'Access-Control-Allow-Origin': request.headers.origin ?? '*',
+        'Access-Control-Allow-Methods': 'POST'
+      }
+    })
   })
 }
